37f5c3d47e52974229fcd68616bd2869b4347ca8
Juno <juno.goh@yitmh.com>
2022-04-12 13:59:50 +0800

    Leetcode 215 Kth largest element in an array.

--- 215-Kth-largest-element-in-an-array.js
+++ 215-Kth-largest-element-in-an-array.js
@@ -16,6 +16,11 @@ Output: 4
  * @param {number} k
  * @return {number}
  */
+/*
+Approach 1: Heap
+Time complexity : O(N log k)
+Space complexity : O(k)
+*/
 class MaxHeap {
   constructor() {
     this.heap = [0];
@@ -104,9 +109,36 @@ var findKthLargest = function (nums, k) {
   return heap.pop();
 };
 
+/*
+Approach 2: Quickselect 
+Time complexity : O(N) in the average case. O(N^2) in the worst case.
+Space complexity : O(1)
+*/
+var findKthLargest = function (nums, k) {
+  k = nums.length - k;
+
+  function quickSelect(l, r) {
+    let [pivot, p] = [nums[r], l];
+
+    for (let i = l; i < r; i++) {
+      if (nums[i] <= pivot) {
+        [nums[p], nums[i]] = [nums[i], nums[p]];
+        p += 1;
+      }
+    }
+    [nums[p], nums[r]] = [nums[r], nums[p]];
+
+    if (p > k) {
+      return quickSelect(l, p - 1);
+    } else if (p < k) {
+      return quickSelect(p + 1, r);
+    } else {
+      return nums[p];
+    }
+  }
+
+  return quickSelect(0, nums.length - 1);
+};
+
 const result = findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4);
 console.log(result);
-/*
-Time complexity : O(N log k)
-Space complexity : O(k)
-*/
